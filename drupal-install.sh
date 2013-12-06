158cfd372d622da2ced6ce6e34dcae75f030e402
Henrik Jepsen <hjep@tv2.dk>
2013-12-06 21:35:46 +0100

    Adding are you sure check and admin email

--- drupal-install.sh
+++ drupal-install.sh
@@ -62,62 +62,70 @@ read drupaladminusernmae
 echo -n "Enter a password for Drupal admin: "
 read drupaladminpassword
 
-echo -e $txtgre"\nDownloading the latest version of drupal\n"$txtrst
-drush dl drupal --drupal-project-rename drupal
-cd drupal
+echo -n "Enter a email for Drupal admin: "
+read drupaladminmail
 
-echo -e $txtgre"\nStating the installation of core\n"$txtrst
-drush si -y --db-url=mysql://$mysqlusernmae:$mysqlpassword@$mysqlhost/$mysqldatabase --account-name=$drupaladminusernmae --account-pass=$drupaladminpassword
+read -p "Start installation? [Y/N] " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo -e $txtgre"\nDownloading the latest version of drupal\n"$txtrst
+  drush dl drupal --drupal-project-rename drupal
+  cd drupal
 
-echo -e $txtgre"\nDownload and set admin theme to Adminimal\n"$txtrst
-drush dl adminimal_theme
-drush variable-set admin_theme adminimal
+  echo -e $txtgre"\nStating the installation of core\n"$txtrst
+  drush si -y --db-url=mysql://$mysqlusernmae:$mysqlpassword@$mysqlhost/$mysqldatabase --account-name=$drupaladminusernmae --account-pass=$drupaladminpassword --account-mail=$drupaladminmail
 
-echo -e $txtgre"\nDisable unusable core modules"$txtrst
-drush dis toolbar overlay contextual -y
+  echo -e $txtgre"\nDownload and set admin theme to Adminimal\n"$txtrst
+  drush dl adminimal_theme
+  drush variable-set admin_theme adminimal
 
-echo -e $txtgre"\nDownload modules and enable them\n"$txtrst
-drush dl ctools devel features entity panels views admin_menu adminimal_admin_menu pathauto strongarm token module_filter link field_group advanced_help libraries
-drush en ctools ctools_custom_content page_manager devel features entity entity_token panels panels_mini views views_ui views_content admin_menu adminimal_admin_menu pathauto strongarm token module_filter link field_group advanced_help libraries -y
+  echo -e $txtgre"\nDisable unusable core modules"$txtrst
+  drush dis toolbar overlay contextual -y
 
-echo -e $txtgre"\nDownload and install Backup and Migrate module\n"$txtrst
-cd sites/all/
-mkdir -p "libraries"
-cd libraries
-mkdir -p "dropbox"
-cd ../../../
-wget https://github.com/BenTheDesigner/Dropbox/archive/master.zip
-tar -xf master.zip
-mv "Dropbox-master/Dropbox/" "sites/all/libraries/dropbox"
-rm -rf Dropbox-master
-rm master.zip
-drush dl backup_migrate backup_migrate_files backup_migrate_dropbox
-drush en backup_migrate backup_migrate_files backup_migrate_dropbox -y
+  echo -e $txtgre"\nDownload modules and enable them\n"$txtrst
+  drush dl ctools devel features entity panels views admin_menu adminimal_admin_menu pathauto strongarm token module_filter link field_group advanced_help libraries
+  drush en ctools ctools_custom_content page_manager devel features entity entity_token panels panels_mini views views_ui views_content admin_menu adminimal_admin_menu pathauto strongarm token module_filter link field_group advanced_help libraries -y
 
-echo -e $txtgre"\nFix for Admin menu and Adminimal menu\n"$txtrst 
-drush variable-set admin_menu_margin_top 0
-drush variable-set adminimal_admin_menu_render "hidden"
+  echo -e $txtgre"\nDownload and install Backup and Migrate module\n"$txtrst
+  cd sites/all/
+  mkdir -p "libraries"
+  cd libraries
+  mkdir -p "dropbox"
+  cd ../../../
+  wget https://github.com/BenTheDesigner/Dropbox/archive/master.zip
+  tar -xf master.zip
+  mv "Dropbox-master/Dropbox/" "sites/all/libraries/dropbox"
+  rm -rf Dropbox-master
+  rm master.zip
+  drush dl backup_migrate backup_migrate_files backup_migrate_dropbox
+  drush en backup_migrate backup_migrate_files backup_migrate_dropbox -y
 
-echo -e $txtgre"\nInstall jQuery Update\n"$txtrst
-drush dl jquery_update-7.x-2.x-dev
-drush en jquery_update -y
-drush variable-set --format="string" jquery_update_jquery_version "1.10"
-drush variable-set --format="string" jquery_update_jquery_admin_version "1.7"
-drush variable-set --format="string" jquery_update_jquery_cdn "google"
-drush variable-set --format="string" jquery_update_compression_type "min"
-drush cc all
+  echo -e $txtgre"\nFix for Admin menu and Adminimal menu\n"$txtrst 
+  drush variable-set admin_menu_margin_top 0
+  drush variable-set adminimal_admin_menu_render "hidden"
 
-echo -e $txtgre"\nInstall Bootstrap theme\n"$txtrst
-drush dl bootstrap
-drush pm-enable bootstrap -y
-drush variable-set theme_default "bootstrap"
+  echo -e $txtgre"\nInstall jQuery Update\n"$txtrst
+  drush dl jquery_update-7.x-2.x-dev
+  drush en jquery_update -y
+  drush variable-set --format="string" jquery_update_jquery_version "1.10"
+  drush variable-set --format="string" jquery_update_jquery_admin_version "1.7"
+  drush variable-set --format="string" jquery_update_jquery_cdn "google"
+  drush variable-set --format="string" jquery_update_compression_type "min"
+  drush cc all
 
-echo -e $txtgre"\nDisable unused themes\n"$txtrst
-drush pm-disable bartik -y
-drush pm-disable seven -y
+  echo -e $txtgre"\nInstall Bootstrap theme\n"$txtrst
+  drush dl bootstrap
+  drush pm-enable bootstrap -y
+  drush variable-set theme_default "bootstrap"
 
-echo -e $txtgre"\nInstallation is finished\n"$txtrst
-echo -e $txtblu"Login information:" 
-echo -e $txtgre"Username:$txtred $drupaladminusernmae"
-echo -e $txtgre"Password:$txtred $drupaladminpassword"
-echo $txtrst
+  echo -e $txtgre"\nDisable unused themes\n"$txtrst
+  drush pm-disable bartik -y
+  drush pm-disable seven -y
+
+  echo -e $txtgre"\nInstallation is finished\n"$txtrst
+  echo -e $txtblu"Login information:" 
+  echo -e $txtgre"Username:$txtred $drupaladminusernmae"
+  echo -e $txtgre"Password:$txtred $drupaladminpassword"
+  echo $txtrst
+fi
